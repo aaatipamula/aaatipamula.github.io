a7bbb8cb91eca63e7ea071b5c252ea46aa40af39
Aniketh Aaatipamula <96833094+aaatipamula@users.noreply.github.com>
2024-03-10 12:30:11 -0500

    Dev (#11)
    * update music link generation to use oembeds and a card carousel instead of iframes
    
    * make cards clickable, nicer transitions, add iframe element, added support for even numbers of cards, updated some content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// General purpose function to hide element children
 function hideChildren(elemID) {
   const elem = document.getElementById(elemID);
   for (const child of elem.children) {
@@ -5,19 +6,17 @@ function hideChildren(elemID) {
   }
 }
 
-// timeout function
+// Timeout function
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-// get random int in range
-// here for posterity
+// Get random int in range (unused currently)
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-// animate my signature
+// Animate my signature
 async function animateSignature(signatureID, durationMultiplier) {
   const signature = document.getElementById(signatureID);
 
@@ -26,7 +25,7 @@ async function animateSignature(signatureID, durationMultiplier) {
     child.style.visibility = 'visible'
 
     let signatureLen = child.getTotalLength();
-    let duration = Math.floor(signatureLen * durationMultiplier)
+    let duration = Math.floor(signatureLen * durationMultiplier);
 
     child.style.strokeDasharray = signatureLen + ' ' + signatureLen;
     child.style.strokeDashoffset = signatureLen;
@@ -34,55 +33,135 @@ async function animateSignature(signatureID, durationMultiplier) {
     let animation = child.animate(
       [{strokeDashoffset: signatureLen}, {strokeDashoffset: 0}],
       {duration: duration, iterations: 1}
-    )
+    );
 
-    animation.onfinish = (event) => {
+    animation.onfinish = () => {
       child.style.strokeDasharray = 0;
       child.style.strokeDashoffset = 0;
     }
 
-    await sleep(duration)
+    await sleep(duration);
   }
 
-  for (const child of signature.getElementsByTagName('circle')) {
-    child.style.visibility = 'visible'
-  }
+  signature.querySelector('circle').style.visibility = 'visible';
 }
 
-// dynamically add in divs to add iframes in for spotify songs
-function spotifyFrames(addBeforeID) {
+// Dynamically add a card carousel and iframe for all the Spotify links that exist
+async function spotifyFrames(addBeforeID) {
+  // Create all of our DOM elments 
   const musicDiv = document.createElement("div");
+  const carousel = document.createElement("div");
   const subHead = document.createElement("h2");
-  const headText = document.createTextNode("Linked Music");
 
-  musicDiv.id = "linked-music"
+  musicDiv.id = "linked-music";
+  carousel.classList.add("carousel-container");
+  subHead.innerText = "Linked Music";
 
-  subHead.appendChild(headText)
-  musicDiv.appendChild(subHead)
+  let controlSpan = document.createElement("span");
+  controlSpan.classList.add("control-container")
 
+  let back = document.createElement('i');
+  back.classList.add("fa-solid", "fa-arrow-left", "fa-xl", "carousel-control");
+  back.id = "carousel-back";
+  controlSpan.appendChild(back)
+
+  let forward = document.createElement('i');
+  forward.classList.add("fa-solid", "fa-arrow-right", "fa-xl", "carousel-control");
+  forward.id = "carousel-forward";
+  controlSpan.appendChild(forward)
+
+  let child = document.getElementById(addBeforeID);
+  child.parentNode.insertBefore(subHead, child);
+  child.parentNode.insertBefore(musicDiv, child);
+
+  let frame = document.createElement("iframe");
+  frame.height = 152;
+  frame.allowFullscreen = false;
+  frame.allow = "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture";
+  frame.loading = "lazy";
+ 
+  musicDiv.appendChild(controlSpan);
+  musicDiv.appendChild(carousel);
+  musicDiv.appendChild(frame)
+
+  let itemCount = 0;
+  const carouselItems = []
+
+  // Populate the carousel with cards 
   for (const link of document.links) {
     if (link.hostname.startsWith("open.spotify.com")) {
-      let spotifyFrame = document.createElement("iframe");
-      let songID = link.pathname.substring(link.pathname.lastIndexOf('/') + 1);
+      var songID = link.pathname.substring(link.pathname.lastIndexOf('/') + 1);
+      let res = await fetch("https://open.spotify.com/oembed?url=" + link);
+      let songInfo = await res.json();
 
-      spotifyFrame.id = "spotify:track:" + songID
-      spotifyFrame.src = "https://open.spotify.com/embed/track/" + songID + "?utm_source=generator&theme=0"
-      spotifyFrame.height = 152
-      spotifyFrame.allowFullscreen = false
-      spotifyFrame.allow = "autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      spotifyFrame.loading = "lazy"
-      spotifyFrame.style.borderRadius = "12px"
-      spotifyFrame.style.border = 0
+      let card = document.createElement("div");
+      card.classList.add("carousel-item");
+      card.id = "spotify:song:" + songID
 
+      let thumbnail = new Image(songInfo.thumbnail_width, songInfo.thumbnail_height);
+      thumbnail.src = songInfo.thumbnail_url;
+      thumbnail.style.borderRadius = ".25rem"
 
-      musicDiv.appendChild(spotifyFrame)
+      let text = document.createTextNode(songInfo.title)
+
+      card.appendChild(thumbnail);
+      card.appendChild(text);
+      
+      carousel.appendChild(card);
+      carouselItems.push(card);
+      itemCount++;
     }
   }
 
-  const parent = document.getElementById(addBeforeID).parentNode;
-  let child = document.getElementById(addBeforeID);
+  carouselItems[carouselItems.length - 1].style.marginRight = "0px";
 
-  parent.insertBefore(musicDiv, child)
+  // Assumes constant size for all elements (refer to ext.sass for margin size)
+  // TODO: Work on changing this to be a little more dynamic
+  let itemWidth = carousel.firstChild.getBoundingClientRect().width + 20;
+  let currentIndex = Math.floor(itemCount/2);
+  let offsetWidth = currentIndex * itemWidth;
+
+  // There is a slightly different offset when there are an even number of cards
+  if (itemCount % 2 === 0) {
+    carousel.style.transform = `translateX(${-itemWidth/2}px)`;
+    offsetWidth -= itemWidth/2;
+  }
+
+  // Reload our iframe based off what index we are at
+  function loadFrame(index) {
+    let tag = carouselItems[index].id;
+    songID = tag.split(":")[2];
+    frame.src = "https://open.spotify.com/embed/track/" + songID + "?utm_source=generator&theme=0";
+  }
+
+  // Go to a specific carousel card
+  function goToIndex(index) {
+    if (index < 0) {
+      index = itemCount - 1;
+    } else if (index >= itemCount) {
+      index = 0; 
+    }
+
+    loadFrame(index);
+
+    carouselItems[currentIndex].classList.toggle("carousel-active");
+    carouselItems[index].classList.toggle("carousel-active");
+
+    carousel.style.transform = `translateX(${-(index * itemWidth) + offsetWidth}px)`;
+    currentIndex = index;
+  }
+
+  // Initial load
+  carouselItems[currentIndex].classList.toggle("carousel-active")
+  loadFrame(currentIndex)
+
+  // Make the forward/back elements and carousel card clickable
+  back.addEventListener("click", () => goToIndex(currentIndex - 1));
+  for (let i = 0; i < itemCount; i++) {
+    carouselItems[i].addEventListener("click", () => goToIndex(i));
+  }
+  forward.addEventListener("click", () => goToIndex(currentIndex + 1));
+
 }
 
 function highlightNav() {
